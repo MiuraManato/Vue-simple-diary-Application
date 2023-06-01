a3c7f6b895072c1a16657a24499e80775f16b46f
manato miura <mmiura2351@gmail.com>
2023-06-01 15:10:36 +0900

    Update: 見やすくした

--- api.py
+++ api.py
@@ -10,54 +10,52 @@ CORS(app)
 # アップロードされたファイルを保存するディレクトリ
 # 実行環境に左右されないようにする
 user_path = os.getcwd()
-app.config['UPLOAD_FOLDER'] = user_path + r'\path\to\the\uploads'
+app.config["UPLOAD_FOLDER"] = user_path + r"\path\to\the\uploads"
 
 # 投稿データを保存するファイル
 posts_file = "posts.json"
 
-@app.route('/upload', methods=['POST'])
+# POSTメソッドで投稿されたデータの処理
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        file = None
+    if "file" not in request.files: file = None
     else:
-        file = request.files['file']
-        if file.filename != '':
+        file = request.files["file"]
+        if file.filename != "":
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-    if 'title' not in request.form or 'content' not in request.form:
-        return 'Missing title or content', 400
+    if "title" not in request.form: return ("Missing title", 400)
+    if "content" not in request.form: return ("Missing content", 400)
 
-    title = request.form['title']
-    content = request.form['content']
+    title = request.form["title"]
+    content = request.form["content"]
 
-    post = {'title': title, 'content': content, 'file': filename if file else None}
+    post = {"title": title, "content": content, "file": filename if file else None}
 
-    # Load existing posts and append new one
     if os.path.exists(posts_file):
-        with open(posts_file, 'r') as f:
+        with open(posts_file, "r") as f:
             posts = json.load(f)
     else:
         posts = []
 
     posts.append(post)
 
-    with open(posts_file, 'w') as f:
+    with open(posts_file, "w") as f:
         json.dump(posts, f)
 
-    return 'Post created successfully', 200
+    return ("Post created successfully", 200)
 
-@app.route('/getPosts', methods=['GET'])
+@app.route("/getPosts", methods=["GET"])
 def get_posts():
-    with open(posts_file, 'r') as f:
+    with open(posts_file, "r") as f:
         posts = json.load(f)
     return jsonify(posts)
 
 
-
 @app.errorhandler(500)
 def handle_cors_error(e):
-    return jsonify({'message': 'Internal Server Error'}), 500
+    return jsonify({"message": "Internal Server Error"}), 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
